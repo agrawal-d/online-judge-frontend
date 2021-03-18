516bf003048d1f9e5fdeb68a858bd3e9a6b13f46
Radhesh <radheshsarma29@gmail.com>
2021-03-19 03:06:59 +0530

    Add form for creating assignments

--- src/components/InstructorDashboard.tsx
+++ src/components/InstructorDashboard.tsx
@@ -1,6 +1,6 @@
 import { RouteComponentProps } from "@reach/router";
 import React from "react";
-import {useState} from "react";
+import {useState,useRef} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Container,Row,Col,Card ,Form,Button,InputGroup, FormControl} from "react-bootstrap";
 import NavBar from "./NavBar";
@@ -9,23 +9,31 @@ import axios from "axios";
 import { setGlobalErrors } from "../reducers/globalErrorsReducer";
 export default function InstructorDashboard(props: RouteComponentProps){
     const dispatch = useDispatch();
-    const [newTAEmail,setNewTAEmail] = useState<string>("");
-    function addTA(){
-        if(!newTAEmail){
-            return;
-        }
+    const [newTAEmails,setNewTAEmails] = useState<string>("");
+    const [newStudentEmails,setStudentMails] = useState<string>("");
+    const [name,setName] = useState<string>("");
 
+    function addAll(){
+        var tas = newTAEmails.split(",");
+        console.log(tas);
+
+        var students = newStudentEmails.split(",");
+        console.log(students);
+
+        console.log(name)
         axios
-        .post("/users/check-ta", {ta_id : newTAEmail})
-        .then((res) =>{
-            const data = res.data;
-            console.log(data);
-            if(data.errors){
-                dispatch(setGlobalErrors(data.errors));
-                return;
-            }
-            setNewTAEmail("");
-        })
+        .post("/assignments/add-assignment", { ass_name: name, ta_ids:tas , student_ids: students }).then((res) => {
+          const data = res.data;
+          console.log(data);
+          if (data.errors) {
+            dispatch(setGlobalErrors(data.errors));
+            return;
+          }
+  
+          setNewTAEmails("");
+          setStudentMails("");
+          setName("");
+        });
     }
     return (
         <>
@@ -36,31 +44,37 @@ export default function InstructorDashboard(props: RouteComponentProps){
                     <Col>
                     <Card body>
                         <Form>
-
-                      
                         <Form.Group id = "name">
                         <Form.Label>Name</Form.Label>
-                            <Form.Control type = "text" required/>
+                            <Form.Control type = "text" value = {name} onChange = {(e) => setName(e.target.value)} required/>
                         </Form.Group>
-                        <Form.File 
-                        id="custom-file"
-                        label="List of Students"
-                        custom
-                       />
                    <InputGroup className="mb-3">
+
                   <InputGroup.Prepend>
                     <InputGroup.Text>Add TA by Email</InputGroup.Text>
                   </InputGroup.Prepend>
                 <FormControl 
                 placeholder = "example@hyderabad.bits-pilani.ac.in"
-                value = {newTAEmail}
+                value = {newTAEmails}
                 type = "email"
-                onChange={(e) => setNewTAEmail(e.target.value)}
-                >
-
-                </FormControl>
+                onChange={(e) => setNewTAEmails(e.target.value)}
+                />
                   </InputGroup>
-                         <Button variant="primary">
+
+
+                  <InputGroup className="mb-3">
+
+                        <InputGroup.Prepend>
+                        <InputGroup.Text>Add Students by Email</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl 
+                        placeholder = "example@hyderabad.bits-pilani.ac.in"
+                        value = {newStudentEmails}
+                        type = "email"
+                        onChange={(e) => setStudentMails(e.target.value)}
+                        />
+                        </InputGroup>
+                         <Button variant="primary" onClick = {addAll}>
                             Submit
                         </Button>
                         </Form>
