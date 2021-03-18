57264467956d5f6b4f4b4d8e50ef1037d26180e3
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2021-03-18 13:46:45 +0530

    Setup config for heroku.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,20 @@
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
 const config = {
-  root: "http://localhost:3001", // frontend,
-  apiBase: "http://localhost:3000/api/v0", //backend
+  root: isLocalhost
+    ? "http://localhost:3001"
+    : "https://bits-judge.herokuapp.com", // frontend,
+  apiBase: isLocalhost
+    ? "http://localhost:3000/api/v0"
+    : "http://bits-judge-server.herokuapp.com/api/v0", //backend
 };
 
 export default config;
