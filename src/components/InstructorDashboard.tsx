f987ff209b1bd77552cdcd7af8363142ca73e8e9
Radhesh <radheshsarma29@gmail.com>
2021-03-19 18:21:36 +0530

    add UI for assignment

--- src/components/InstructorDashboard.tsx
+++ src/components/InstructorDashboard.tsx
@@ -1,88 +1,189 @@
-import { RouteComponentProps } from "@reach/router";
+import { Link, RouteComponentProps } from "@reach/router";
 import React from "react";
-import {useState,useRef} from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Container,Row,Col,Card ,Form,Button,InputGroup, FormControl} from "react-bootstrap";
+import { Container, Row, Col, Card, Form, Button, InputGroup, FormControl, Alert, Collapse } from "react-bootstrap";
 import NavBar from "./NavBar";
-import assignments from "./Assignments";
 import axios from "axios";
 import { setGlobalErrors } from "../reducers/globalErrorsReducer";
-export default function InstructorDashboard(props: RouteComponentProps){
-    const dispatch = useDispatch();
-    const [newTAEmails,setNewTAEmails] = useState<string>("");
-    const [newStudentEmails,setStudentMails] = useState<string>("");
-    const [name,setName] = useState<string>("");
+import { start } from "repl";
 
-    function addAll(){
-        var tas = newTAEmails.split(",");
-        console.log(tas);
 
-        var students = newStudentEmails.split(",");
-        console.log(students);
+export default function InstructorDashboard(props: RouteComponentProps) {
+  const dispatch = useDispatch();
+  const [newTAEmails, setNewTAEmails] = useState<string>("");
+  const [newStudentEmails, setStudentMails] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [startDateTime, setStartDateTime] = useState<string>("");
+  const [endDateTime, setEndDateTime] = useState<string>("");
+  const [error, setError] = useState("");
+  const [open, setOpen] = useState(false);
 
-        console.log(name)
-        axios
-        .post("/assignments/add-assignment", { ass_name: name, ta_ids:tas , student_ids: students }).then((res) => {
-          const data = res.data;
-          console.log(data);
-          if (data.errors) {
-            dispatch(setGlobalErrors(data.errors));
-            return;
-          }
-  
-          setNewTAEmails("");
-          setStudentMails("");
-          setName("");
-        });
+  function addAll() {
+    if (!name) {
+      setError("Assignment Name Empty");
+      return;
     }
-    return (
-        <>
-        <div className="home">
-            <Container>
-                <NavBar />
-                <Row>
-                    <Col>
-                    <Card body>
-                        <Form>
-                        <Form.Group id = "name">
+    if (!newTAEmails) {
+      setError("No TA Assigned");
+      return;
+    }
+    if (!newStudentEmails) {
+      setError("No student Added");
+      return;
+    }
+    if(!startDateTime){
+      setError("Set Start Time");
+    }
+    if(!endDateTime){
+      setError("set End Time");
+    }
+    if (startDateTime > endDateTime) {
+      setError("Start Time is After End Time");
+      return;
+    }
+    let tas = newTAEmails.split(",");
+
+    let students = newStudentEmails.split(",");
+
+    axios
+      .post("/assignments/add-assignment", {
+        ass_name: name,
+        ta_ids: tas,
+        student_ids: students,
+        start: startDateTime,
+        end: endDateTime
+      }).then((res) => {
+      const data = res.data;
+      console.log(data);
+      if (data.errors) {
+        dispatch(setGlobalErrors(data.errors));
+        return;
+      }
+
+      setNewTAEmails("");
+      setStudentMails("");
+      setName("");
+      setError("");
+    });
+  }
+
+  return (
+    <>
+      <div className="home">
+        <Container>
+          <NavBar />
+          <Row>
+            <Button
+              onClick={() => setOpen(!open)}
+              aria-controls="example-collapse-text"
+              aria-expanded={open}
+            >
+              Add Assignment
+            </Button>
+          </Row>
+
+          <Row>
+            <Col>
+
+          <Collapse in={open}>
+            <div>
+              <Row>
+                <div>
+                  {error && <Alert variant="danger">{error}</Alert>}
+                </div>
+
+              </Row>
+
+              <Row>
+
+                <Col>
+                  <Card.Header>
+
+                  </Card.Header>
+                  <Card body>
+                    <Form>
+                      <Form.Group id="name">
                         <Form.Label>Name</Form.Label>
-                            <Form.Control type = "text" value = {name} onChange = {(e) => setName(e.target.value)} required/>
-                        </Form.Group>
-                   <InputGroup className="mb-3">
-
-                  <InputGroup.Prepend>
-                    <InputGroup.Text>Add TA by Email</InputGroup.Text>
-                  </InputGroup.Prepend>
-                <FormControl 
-                placeholder = "example@hyderabad.bits-pilani.ac.in"
-                value = {newTAEmails}
-                type = "email"
-                onChange={(e) => setNewTAEmails(e.target.value)}
-                />
-                  </InputGroup>
-
-
-                  <InputGroup className="mb-3">
+                        <Form.Control type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+                      </Form.Group>
+                      <InputGroup className="mb-3">
 
                         <InputGroup.Prepend>
-                        <InputGroup.Text>Add Students by Email</InputGroup.Text>
+                          <InputGroup.Text>Add TA by Email</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <FormControl 
-                        placeholder = "example@hyderabad.bits-pilani.ac.in"
-                        value = {newStudentEmails}
-                        type = "email"
-                        onChange={(e) => setStudentMails(e.target.value)}
+                        <FormControl
+                          placeholder="example@hyderabad.bits-pilani.ac.in"
+                          value={newTAEmails}
+                          type="email"
+                          onChange={(e) => setNewTAEmails(e.target.value)}
                         />
-                        </InputGroup>
-                         <Button variant="primary" onClick = {addAll}>
-                            Submit
-                        </Button>
-                        </Form>
-                    </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-        </>
-    );
+                      </InputGroup>
+
+
+                      <InputGroup className="mb-3">
+
+                        <InputGroup.Prepend>
+                          <InputGroup.Text>Add Students by Email</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                          placeholder="example@hyderabad.bits-pilani.ac.in"
+                          value={newStudentEmails}
+                          type="email"
+                          onChange={(e) => setStudentMails(e.target.value)}
+                        />
+                      </InputGroup>
+
+                      <InputGroup className="mb-3">
+
+                        <InputGroup.Prepend>
+                          <InputGroup.Text>Start Date</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                          type="datetime-local"
+                          value={startDateTime}
+                          onChange={(e) => setStartDateTime(e.target.value)}
+                        />
+                      </InputGroup>
+
+                      <InputGroup className="mb-3">
+
+                        <InputGroup.Prepend>
+                          <InputGroup.Text>End Date</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                          type="datetime-local"
+                          value={endDateTime}
+                          onChange={(e) => setEndDateTime(e.target.value)}
+                        />
+                      </InputGroup>
+
+
+                      <Button variant="primary" onClick={addAll}>
+                        Submit
+                      </Button>
+                    </Form>
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          </Collapse>
+
+            </Col>
+            <Col>
+              <Col>
+                <h3>Admin actions</h3>
+                <ul>
+                  <li>
+                    <Link to="/user-setup">Setup Hacker Earth Credentials</Link>
+                  </li>
+                </ul>
+              </Col>
+            </Col>
+          </Row>
+
+        </Container>
+      </div>
+    </>
+  );
 }
