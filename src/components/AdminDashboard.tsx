5c8ab542b7d994b17644087646518527b3e7fd68
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2021-04-27 13:33:30 +0530

    Dash

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -3,6 +3,8 @@ import React, { ReactElement, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser } from "../reducers/userReducer";
 import { Link } from "@reach/router";
+import faker from "faker";
+
 import {
   Container,
   Row,
@@ -10,6 +12,7 @@ import {
   Button,
   Alert,
   Card,
+  Badge,
   InputGroup,
   FormControl,
 } from "react-bootstrap";
@@ -17,6 +20,63 @@ import Loading from "./Loading";
 import { User } from "../types";
 import NavBar from "./NavBar";
 import { setGlobalErrors } from "../reducers/globalErrorsReducer";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+
+const data = [
+  { name: "January", uv: 400, pv: 2400, amt: 2400 },
+  { name: "February", uv: 100, pv: 2400, amt: 2400 },
+  { name: "March", uv: 700, pv: 2400, amt: 2400 },
+  { name: "April", uv: 900, pv: 2400, amt: 2400 },
+];
+
+const pieData = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+  { name: "Group E", value: 278 },
+  { name: "Group F", value: 189 },
+];
+
+const renderLineChart = (
+  <div>
+    <LineChart
+      width={600}
+      height={300}
+      data={data}
+      margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+    >
+      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+    </LineChart>
+    <h4 className="text-center">Monthly Usage statistics</h4>
+  </div>
+);
+
+const renderPieChart = (
+  <ResponsiveContainer width="100%" height="100%">
+    <PieChart width={400} height={400}>
+      <Pie
+        dataKey="value"
+        data={pieData}
+        outerRadius={80}
+        fill="#8884d8"
+        label
+      />
+    </PieChart>
+  </ResponsiveContainer>
+);
 
 export default function AdminDashboard() {
   const user = useSelector(selectUser) as User;
@@ -81,7 +141,7 @@ export default function AdminDashboard() {
             <Col md="auto">
               <img
                 src={user.picture}
-                className="d-block"
+                className="d-block b"
                 alt="Profile"
                 width="40"
               />
@@ -138,6 +198,83 @@ export default function AdminDashboard() {
                 </InputGroup>
                 <Button onClick={createNewInstructor}>Save</Button>
               </Card>
+            </Col>
+          </Row>
+          <h1 className="text-center">Overview</h1>
+          <hr />
+          <Row>
+            <Col>
+              <Card body>{renderLineChart}</Card>
+            </Col>
+            <Col>
+              <Card body>
+                <img
+                  src="pie-chart.png"
+                  alt="pie chart"
+                  className="pad-10 block"
+                />
+                <br />
+                <h4 className="text-center">User Demographics</h4>
+              </Card>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col>
+              <Card bg="primary" text="light" body>
+                <h1>{faker.datatype.number(10000)}</h1>
+                <h4>Users</h4>
+              </Card>
+            </Col>
+            <Col>
+              <Card bg="warning" text="light" body>
+                <h1>{faker.datatype.number(500)}</h1>
+                <h4>Assignments</h4>
+              </Card>
+            </Col>
+            <Col>
+              <Card bg="info" text="light" body>
+                <h1>{faker.datatype.number(5000)}</h1>
+                <h4>Students</h4>
+              </Card>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col>
+              <Card className="he-bg" text="light" body>
+                <h1>
+                  {faker.datatype.number(10000)}
+
+                  <img
+                    src="https://miro.medium.com/max/600/1*_qEyZ2IU41N3LwrWXV43qg.jpeg"
+                    alt="he"
+                    width={200}
+                  />
+                </h1>
+
+                <h4>HackerEarth API Calls Usage</h4>
+              </Card>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col>
+              <h2>Service Status</h2>
+              <hr />
+              <Button variant="primary">
+                API <Badge variant="light">Online</Badge>
+              </Button>
+              <Button variant="success">
+                HackerEarth <Badge variant="light">Online</Badge>
+              </Button>
+              <Button variant="warning">
+                Client Server <Badge variant="light">Online</Badge>
+              </Button>
+              <Button variant="info">
+                MongoDB Database <Badge variant="light">Online</Badge>
+              </Button>
+              .
             </Col>
           </Row>
         </Container>
